04ea3c8a1134bf1dc0cf7cf4b8f85b8927dcfa13
Sergiy Humenniy <gum0372@gmail.com>
2024-08-16 17:38:21 +0300

    all

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesItem = document.querySelectorAll('li.item');
-
-console.log('Numbers of categories :', categoriesItem.length);
-
-categoriesItem.forEach(item => {
-  console.log(`Category:`, item.querySelector('h2').textContent);
-
-  console.log(`Elements:`, item.querySelectorAll('ul li').length);
-});
--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', event => {
-  const inputText = event.target.value.trim();
-
-  nameOutput.textContent = inputText === '' ? 'Anonymous' : inputText;
-});
